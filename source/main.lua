72921dc3b8d4a9d58ec861a3d5b69af37a63e3fc
Jessica Stojkovic <jrstojkovic123@gmail.com>
2022-08-23 16:09:21 -0600

    can now edit individual values with d-pad

--- source/main.lua
+++ source/main.lua
@@ -7,10 +7,13 @@ import "Corelibs/graphics"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
-local hp = 40
-local cd1 = 0
-local cd2 = 0
-local cd3 = 0
+
+-- value variables
+local values = {40,0,0,0}
+print(values[1])
+-- navigation variables
+local selected = 1
+local edit = -1
 local fontNontendoBold2X = gfx.font.new('font/Nontendo-Bold-2x')
 local fontNontendoBold8X = gfx.font.new('font/Nontendo-Bold-8x')
 local fontNontendoBold6X = gfx.font.new('font/Nontendo-Bold-6x')
@@ -18,7 +21,12 @@ local fontNontendoBold6X = gfx.font.new('font/Nontendo-Bold-6x')
 gfx.setLineCapStyle(gfx.kLineCapStyleRound)
 gfx.setLineWidth(5)
 
+
 function updateScreen()
+	hp = values[1]
+	cd1 = values[2]
+	cd2 = values[3]
+	cd3 = values[4]
 	gfx.clear()
 	gfx.setFont(fontNontendoBold2X)
 	gfx.drawText("Life",10,10)
@@ -35,10 +43,16 @@ function updateScreen()
 		gfx.drawArc(200,73,63,0,360)
 		gfx.drawArc(200,73,66,0,9*(hp-80))
 	end
+	if(selected == 1) then
+		gfx.drawLine(125,145,275,145)
+	elseif (selected == 2) then
+		gfx.drawLine(75,230,125,230)
+	elseif (selected == 3) then
+		gfx.drawLine(175,230,225,230)
+	elseif (selected == 4) then
+		gfx.drawLine(275,230,325,230)
+	end
 	
-	
-	--gfx.drawText(hpText, 160,30)
-	gfx.drawLine(125,145,275,145)
 
 	--commander damage zone
 
@@ -46,19 +60,42 @@ function updateScreen()
 	--gfx.drawText("Commander Damage",90,160)
 	gfx.drawTextAligned("Commander Damage", 200, 160, kTextAlignment.center)
 	gfx.drawRoundRect(10,150,380,170,10)
-	gfx.drawText(cd1, 100,200)
-	gfx.drawText(cd2, 190,200)
-	gfx.drawText(cd3, 280,200)
+	gfx.drawTextAligned(cd1, 100, 205, kTextAlignment.center)
+	gfx.drawTextAligned(cd2, 200, 205, kTextAlignment.center)
+	gfx.drawTextAligned(cd3, 300, 205, kTextAlignment.center)
+	--gfx.drawText(cd1, 100,200)
+	--gfx.drawText(cd2, 190,200)
+	--gfx.drawText(cd3, 280,200)
 
 end
 updateScreen()
 function playdate.update()
 	if pd.buttonJustPressed(pd.kButtonUp) then
-		hp += 1
+		if edit ~= -1 then
+			values[selected] += 1
+		elseif selected ~= 1 then
+			selected = 1
+		end
 		updateScreen()
-	elseif pd.buttonJustPressed(pd.kButtonDown) then
-		hp -= 1
+	elseif (pd.buttonJustPressed(pd.kButtonDown)) and (selected == 1) then
+		if edit ~= -1 then
+			values[selected] -= 1
+		else
+			selected = 3
+		end
 		updateScreen()
+	elseif (pd.buttonJustPressed(pd.kButtonLeft)) and ((selected == 3) or (selected  == 4)) then
+		selected -= 1
+		edit = -1
+		updateScreen()
+	elseif (pd.buttonJustPressed(pd.kButtonRight)) and ((selected == 3 ) or (selected == 2)) then
+		selected += 1
+		edit = -1
+		updateScreen()
+	elseif (pd.buttonJustPressed(pd.kButtonA)) then
+		edit = selected
+	elseif (pd.buttonJustPressed(pd.kButtonB)) then
+		edit = -1
 	end
 end
 
