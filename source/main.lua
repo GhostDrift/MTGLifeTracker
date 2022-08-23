524cd86c71e2937b52dfae8e975cc75582edc335
Jessica Stojkovic <jrstojkovic123@gmail.com>
2022-08-23 09:48:17 -0600

    added two other comander damage trackers

--- source/main.lua
+++ source/main.lua
@@ -8,7 +8,8 @@ import "Corelibs/graphics"
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 local fnt <const> = gfx.font
-local cellNumber = 40
+local hp = 40
+local cd = 20
 local fontNontendoBold2X = gfx.font.new('font/Nontendo-Bold-2x')
 local fontNontendoBold8X = gfx.font.new('font/Nontendo-Bold-8x')
 function updateScreen()
@@ -16,15 +17,18 @@ function updateScreen()
 	gfx.setFont(fontNontendoBold2X)
 	gfx.drawText("Life",10,10)
 	gfx.setFont(fontNontendoBold8X)
-	local cellText = tostring(cellNumber)
-	local textWidth = fontNontendoBold8X:getTextWidth(cellText)
-	local cellRadius = textWidth /1.4
-	gfx.drawCircleAtPoint(195,65,cellRadius)
-	gfx.drawText(cellText,150,25)
+	local hpText = tostring(hp)
+	--local textWidth = fontNontendoBold8X:getTextWidth(cellText)
+	--local cellRadius = textWidth /1.4
+	--gfx.drawCircleAtPoint(195,65,cellRadius)
+	gfx.drawText(hpText,150,25)
 	gfx.setFont(fontNontendoBold2X)
 	gfx.drawText("Commander Damage",90,140)
 	
-	gfx.drawText("0", 40,180)
+	gfx.drawText(cd, 120,180)
+	gfx.drawText(cd, 180,180)
+	gfx.drawText(cd, 240,180)
+
 end
 updateScreen()
 function playdate.update()
